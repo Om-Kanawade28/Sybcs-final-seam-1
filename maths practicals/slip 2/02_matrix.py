6d558a7aed5c85c531069ce867e2e41515c4593d
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-11-30 16:44:10 +0530

    Add files via upload

---
+++ maths practicals/slip 2/02_matrix.py
@@ -0,0 +1,17 @@
+import sympy
+from sympy import Matrix
+
+A = Matrix([[1,0,5],[2,1,6],[3,4,0]])
+B = Matrix([[2,5],[-1,4]])
+
+try:
+    print("A + B=",A + B)
+    print("A - B=",A - B)
+except (ValueError, TypeError) as e:
+    print(f"Error: {e}")
+
+print("Inverse of A=",A.inv())
+print("Determinant of A=",A.det())
+print("Inverse of B=",B.inv())
+print("Determinant of B=",B.det())
+
