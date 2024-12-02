b1cf5e0108d7165ef1572a8d2e1086f011c1c6be
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-02 17:04:38 +0530

    Add files via upload

---
+++ maths practicals/slip 7/03_eigen_values_vectors.py
@@ -0,0 +1,7 @@
+from sympy import *
+A = Matrix([[1,3,3],[2,2,3],[4,2,1]])
+print("eigenvalues==",A.eigenvals())
+print("eigenvectors ==",A.eigenvects())
+P,D = A.diagonalize()
+print("P==",P)
+print("D===",D)
