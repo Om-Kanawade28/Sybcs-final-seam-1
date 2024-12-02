77b9428805798855391172b65e1f5541f8c3a1f4
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-02 17:05:50 +0530

    Add files via upload

---
+++ maths practicals/slip 7/05_plot_rectangle.py
@@ -0,0 +1,9 @@
+#) Plot a rectangle of side 100 and 150 pixels starting with it’s current position.
+import turtle
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(150)
